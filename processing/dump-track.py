d1daa0cae09acddac126ca4b0f41597b33522f5d
Yury D. Veksler <TheOwlSoul@gmail.com>
2020-02-23 18:55:30 +0300

    GPGSA messages parsing complete

--- processing/dump-track.py
+++ processing/dump-track.py
@@ -15,7 +15,7 @@ def nmea_to_degrees(data):
 
 def read_gprmc(sentence, timestamp, do_print=False):
     """ Read and parse GPRMC message"""
-    values = sentence.split(',')
+    values = sentence.split('*')[0].split(',')
     result = {}
 
     # Linux timestamp
@@ -116,7 +116,7 @@ def read_gprmc(sentence, timestamp, do_print=False):
 
 def read_gpgga(sentence, timestamp, do_print=False):
     """ Read and parse GPGGA message"""
-    values = sentence.split(',')
+    values = sentence.split('*')[0].split(',')
     result = {}
 
     # Linux timestamp
@@ -211,10 +211,163 @@ def read_gpgga(sentence, timestamp, do_print=False):
     
     return result
 
+def read_gpvtg(sentence, timestamp, do_print=False):
+    """ Read and parse GPVTG message"""
+    values = sentence.split('*')[0].split(',')
+    result = {}
+
+    # Linux timestamp
+    try:
+        result['linux_stamp'] = int(timestamp)
+    except:
+        result['linux_stamp'] = 0
+    result['linux_date'] = datetime.fromtimestamp(result['linux_stamp'], tz=pytz.UTC)
+
+    # True Track
+    try:
+        result['truetrack'] = float(values[1])
+    except:
+        result['truetrack'] = None
+
+    # Magnetic Track
+    try:
+        result['magnetictrack'] = float(values[3])
+    except:
+        result['magnetictrack'] = None
+
+    # Groundspeed, knots
+    try:
+        result['groundspeed_knots'] = float(values[5])
+    except:
+        result['groundspeed_knots'] = None
+    
+    # Groundspeed, km/h
+    try:
+        result['groundspeed_kmh'] = float(values[7])
+    except:
+        result['groundspeed_kmh'] = None
+
+    if do_print:
+        if result['truetrack'] is None and \
+           result['magnetictrack'] is None and \
+           result['groundspeed_knots'] is None and \
+           result['groundspeed_kmh'] is None:
+            pass
+        else:
+            print("Linux timestamp   :", result['linux_stamp'])
+            print("Linux datetime    :", result['linux_date'])
+            print("  True Track      :", result['truetrack'])
+            print("  Magnetic Track  :", result['magnetictrack'])
+            print("  Ground spd, knot:", result['groundspeed_knots'])
+            print("  Ground spd, km/h:", result['groundspeed_kmh'])
+            print("")
+    
+    return result
+
+def read_gpgsa(sentence, timestamp, do_print=False):
+    """ Read and parse GPGSA message"""
+    values = sentence.split('*')[0].split(',')
+    result = {}
+
+    # Linux timestamp
+    try:
+        result['linux_stamp'] = int(timestamp)
+    except:
+        result['linux_stamp'] = 0
+    result['linux_date'] = datetime.fromtimestamp(result['linux_stamp'], tz=pytz.UTC)
+
+    # Fix selection
+    try:
+        result['fix_selection'] = str(values[1])
+    except:
+        result['fix_selection'] = None
+
+    # Fix type
+    try:
+        result['fix_type'] = int(values[2])
+    except:
+        result['fix_type'] = None
+
+    # Fix satellites
+    try:
+        result['fix_satellites'] = []
+        for sat in range(3, 3+12):
+            try:
+                sat_no = int(values[sat])
+                result['fix_satellites'].append(sat_no)
+            except Exception as e:
+                pass
+    except:
+        result['fix_satellites'] = None
+
+    # Satellite counts
+    if result['fix_satellites'] is None:
+        result['fix_sat_count'] = 0
+    else:
+        result['fix_sat_count'] = len(result['fix_satellites'])
+
+    # PDOP
+    try:
+        result['pdop'] = float(values[15])
+    except:
+        result['pdop'] = None
+
+    # HDOP
+    try:
+        result['hdop'] = float(values[16])
+    except:
+        result['hdop'] = None
+
+    # VDOP
+    try:
+        result['vdop'] = float(values[17])
+    except:
+        result['vdop'] = None
+    if do_print:
+        print("Linux timestamp   :", result['linux_stamp'])
+        print("Linux datetime    :", result['linux_date'])
+        print("  Fix selection   :", result['fix_selection'])
+        print("  Fix type        :", result['fix_type'])
+        print("  Satellites count:", result['fix_sat_count'])
+        print("  Fix satellites  :", end=" ")
+        if result['fix_satellites'] == None:
+            print("NONE")
+        elif len(result['fix_satellites']) == 0:
+            print("EMPTY")
+        else:
+            for sat in result['fix_satellites']:
+                print(sat, end=" ")
+            print()       
+        print("  PDOP            :", result['pdop'])
+        print("  HDOP            :", result['hdop'])
+        print("  VDOP            :", result['vdop'])
+        print()
+    
+    return result
+
+def read_gpgsv(sentence, timestamp, do_print=False):
+    """ Read and parse GPGSV message"""
+    values = sentence.split(',')
+    result = {}
+
+    # Linux timestamp
+    try:
+        result['linux_stamp'] = int(timestamp)
+    except:
+        result['linux_stamp'] = 0
+    result['linux_date'] = datetime.fromtimestamp(result['linux_stamp'], tz=pytz.UTC)
+
+    if do_print:
+        print("Linux timestamp   :", result['linux_stamp'])
+        print("Linux datetime    :", result['linux_date'])
+        print("")
+    
+    return result
+
 if __name__ == '__main__':
     totaldata = {}
     with open(INPUT_FILE, "r") as f:
-        for line in f.readlines():s
+        for line in f.readlines():
             try:
                 sentence = line.strip().split(';')
                 if not sentence[0] in totaldata:
@@ -230,12 +383,49 @@ if __name__ == '__main__':
                 if sentence[1].startswith("$GPGGA"):
                     gpgga = read_gpgga(sentence[1], sentence[0], do_print=False)
                     totaldata[sentence[0]]['gpgga'].append(gpgga)
+                if sentence[1].startswith("$GPVTG"):
+                    gpvtg = read_gpvtg(sentence[1], sentence[0], do_print=False)
+                    totaldata[sentence[0]]['gpvtg'].append(gpvtg)
+                if sentence[1].startswith("$GPGSA"):
+                    gpgsa = read_gpgsa(sentence[1], sentence[0], do_print=True)
+                    totaldata[sentence[0]]['gpgsa'].append(gpgsa)
+                if sentence[1].startswith("$GPGSV"):
+                    gpgsv = read_gpgsv(sentence[1], sentence[0], do_print=False)
+                    totaldata[sentence[0]]['gpgsv'].append(gpgsv)
             except Exception as e:
                 pass
     
-    print("Track contains " + str(len(totaldata)) + " entries")
-    for key, entry in totaldata.items():
-        print(key, entry)
-        print()
+    print()
+    print("TRACK ANALYSIS:")
 
+    # Calculate valid datapoints
+    valid_points = 0
+    for key, entry in totaldata.items():
+        if (not entry['gpgga'] is None):
+            for record in entry['gpgga']:
+                if (not record['fix'] is None) and record['fix'] == 1:
+                    valid_points += 1
+
+    # Fix percentage
+    fix_percentage = {0: 0, 1: 0, 2: 0, 3: 0}
+    for key, entry in totaldata.items():
+        if (not entry['gpgsa'] is None):
+            for record in entry['gpgsa']:
+                if (not record['fix_type'] is None):
+                    if record['fix_type'] in fix_percentage:
+                        fix_percentage[record['fix_type']] += 1
+
+
+    # Print analytics
+    print(" - Track entries    (timestamped)   : " + str(len(totaldata)))
+    print(" - Valid datapoints (with GNSS fix) : " + str(valid_points))
+    print("              NO FIX points         : " + str(fix_percentage[1]))
+    print("              2D FIX points         : " + str(fix_percentage[2]))
+    print("              3D FIX points         : " + str(fix_percentage[3]))
+    
+    #for key, entry in totaldata.items():
+    #    print(key, entry)
+    #    print()
+
+    print()
     print("Track dumped successfully!")
